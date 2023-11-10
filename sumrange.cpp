fa1516255214252490b8a7a888302d55eb071a1d
Arshaq Ahmed <arshahm2004@gmail.com>
2023-11-10 10:43:17 +0000

    initial commit

---
+++ sumrange.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(); {
+
+int x, y;
+cout << "Enter two numbers: ";
+cin >> x >> y;
+
+int sum = x + y;
+cout << sum << endl;
+
+return 0;
+}
